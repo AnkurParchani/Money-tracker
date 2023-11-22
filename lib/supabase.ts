3b45ca14f4b9adb12be2c69bf03a9c312ca0ab29
Ankur Parchani <ankurparchani@gmail.com>
2023-11-22 21:15:18 +0530

    fix: supabase key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env.SUPBASE_KEY);
+
 export const supabaseUrl = "https://rukhkymdgvgdpdajgisu.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey as string);
 
 export default supabase;
