a9d8f28b2b27d5f883ad2cc2089792ab5be32993
Ankur Parchani <ankurparchani@gmail.com>
2023-11-24 18:39:41 +0530

    greeting bug

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,11 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Greeting = ({ name }: { name: string }) => {
+  const [greet, setGreet] = useState<string>("");
   const firstName = name.split(" ")[0];
+
   const hour = new Date().getHours();
 
-  let greet;
-  if (hour >= 0 && hour <= 12) greet = "Good Morning";
-  if (hour > 12 && hour <= 16) greet = "Good Afternoon";
-  if (hour > 16 && hour <= 24) greet = "Good Evening";
+  useEffect(() => {
+    if (hour >= 0 && hour < 12) setGreet("Good Morning");
+    if (hour >= 12 && hour < 16) setGreet("Good Afternoon");
+    if (hour >= 16 && hour <= 24) setGreet("Good Evening");
+  }, [hour]);
 
   return (
     <div className="text-base px-2 text-gray-800 font-medium">
